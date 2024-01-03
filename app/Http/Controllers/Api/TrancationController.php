e79fae3b1dddf8dfe1e2beb1f74b5862cfdbd4f8
Faisal Bahari <fbhr1112@gmail.com>
2024-01-03 16:38:22 +0700

    feat: base controller

--- app/Http/Controllers/Api/TrancationController.php
+++ app/Http/Controllers/Api/TrancationController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\BaseController as BaseController;
 use Illuminate\Http\Request;
 
-class TrancationController extends Controller
+class TrancationController extends BaseController
 {
     //
 }
