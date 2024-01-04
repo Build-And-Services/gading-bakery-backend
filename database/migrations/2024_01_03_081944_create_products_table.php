099b306e511d7373ebef1bf6a9fc99387343c775
Faisal Bahari <fbhr1112@gmail.com>
2024-01-04 12:25:16 +0700

    fix(products): fix migration

--- database/migrations/2024_01_03_081944_create_products_table.php
+++ database/migrations/2024_01_03_081944_create_products_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -22,7 +21,7 @@ return new class extends Migration
             $table->integer('purchase_price');
             $table->integer('selling_price');
             $table->unsignedBigInteger('category_id')->nullable();
-            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('set null');
             $table->timestamps();
         });
     }
