61c8e127fd592eb7e6417d470987a2a915b3929d
rafikusdiarto <rafikusdiarto1707@gmail.com>
2024-01-04 17:48:24 +0700

    fix bug middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -20,15 +20,4 @@ class Authenticate extends Middleware
             return route('login');
         }
     }
-
-    public function render($request, Exception $exception)
-{
-    if ($exception instanceof AuthorizationException) {
-        return response()->json([
-         'message' => 'Not authenticated'
-        ],401);
-    }
-
-    return parent::render($request, $exception);
-}
 }
