5b04db0b6ef6d01d31633abcdeb23ee0b4795e67
rafikusdiarto <rafikusdiarto1707@gmail.com>
2024-01-04 07:23:06 +0700

    feat:crud categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,10 +2,67 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Api\BaseController as BaseController;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Resources\CategoryResource;
+use App\Http\Controllers\Api\BaseController as BaseController;
 
 class CategoryController extends BaseController
 {
-    //
+    public function index()
+    {
+        try {
+            $categories = Category::with('product')
+            ->get();
+            return $this->sendResponse(CategoryResource::collection($categories), "Successfully get data", 200);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), 400);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required',
+            ]);
+            $categories = Category::create([
+                'name' => $request->name,
+            ]);
+            return $this->sendResponse(new CategoryResource($categories), 'Categories created successfully', 201);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), 400);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $request->validate([
+                'name' => 'required',
+            ]);
+
+            $categories = Category::findOrFail($id);
+
+            $dataToUpdate = [
+                'name' => $request->name,
+            ];
+
+            $categories->update($dataToUpdate);
+            return $this->sendResponse(new CategoryResource($categories), 'Successfully updated category', 202);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), 400);
+        }
+    }
+
+    public function destroy(string $id)
+    {
+        try {
+            $categories = Category::findOrFail($id);
+            $categories->delete();
+            return $this->sendResponse(new CategoryResource($categories), 'Successfully deleted category', 203);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), 400);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -40,29 +40,7 @@ Route::prefix('/v1')->group(function () {
         });
     });
 
-    Route::prefix('/categories')->group(function () {
-        // isi sesua controller masing masing
-        Route::get('/', function () {
-            return json_encode([
-                "message" => "hallo",
-            ]);
-        });
-        Route::post('/', function () {
-            return json_encode([
-                "message" => "hallo",
-            ]);
-        });
-        Route::put('/:id', function () {
-            return json_encode([
-                "message" => "hallo",
-            ]);
-        });
-        Route::delete('/:id', function () {
-            return json_encode([
-                "message" => "hallo",
-            ]);
-        });
-    });
+    require __DIR__ . './category.php';
 
     Route::prefix('/stock')->group(function () {
         // isi sesua controller masing masing
