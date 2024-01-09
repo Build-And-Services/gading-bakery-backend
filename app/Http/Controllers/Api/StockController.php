0420fe4e9c93cc56ece272d95e6a432a16e6f4f8
Faisal Bahari <fbhr1112@gmail.com>
2024-01-09 14:45:08 +0700

    feat(stocks): get all stocks and product stocks

--- app/Http/Controllers/Api/StockController.php
+++ app/Http/Controllers/Api/StockController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Resources\ProductResource;
 use App\Http\Resources\StockResource;
 use App\Models\Product;
 use App\Models\Stock;
@@ -10,23 +11,24 @@ use App\Http\Controllers\Api\BaseController as BaseController;
 
 class StockController extends BaseController
 {
-    public function index()
+    public function getAllStocks()
     {
         $stocks = Stock::all();
         return $this->sendResponse(StockResource::collection($stocks), "Successfully get data", 200);
     }
-    public function create(Request $request)
+
+    public function store(Request $request)
     {
         try {
             $request->validate([
                 'quantity' => 'required|integer|min:1',
                 'type' => 'required',
-                'product_id' => 'required',
+                'product_id' => 'required|exists:products,id',
             ]);
 
             if ($request->type == 'decrease') {
                 $product = Product::findOrFail($request->product_id);
-                $decreaseQuantity = $product->getTotalDecreaseQuantity($request->quantity) + $request->quantity;
+                $decreaseQuantity = $product->getTotalDecreaseQuantity() + $request->quantity;
                 if ($decreaseQuantity > $product->getTotalIncreaseQuantity()) {
                     return $this->sendError('Amount exceeds the limit', 400);
                 }
@@ -34,6 +36,27 @@ class StockController extends BaseController
 
             $stock = Stock::create($request->all());
             return $this->sendResponse(new StockResource($stock), 'Successfully add stocks', 201);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), 400);
+        }
+    }
+
+    public function getProductStocks(string $id)
+    {
+        try {
+            $product = Product::findOrFail($id);
+            $stockHistory = $product->stocks()->get();
+
+            $increaseQuantity = $product->getTotalIncreaseQuantity();
+            $decreaseQuantity = $product->getTotalDecreaseQuantity();
+            $totalQuantity = $increaseQuantity - $decreaseQuantity;
+
+            return $this->sendResponse([
+                'product' => $product,
+                'stocks' => $stockHistory,
+                'totalQuantity' => $totalQuantity,
+                'totalResidual' => $totalQuantity * $product->purchase_price
+            ], 'Successfully get product stocks', 200);
         } catch (\Exception $e) {
             return $this->sendError($e->getMessage(), 400);
         }
