6563fc6b37d39431351c3df3eb3a78b9c2d5e93e
rafikusdiarto <rafikusdiarto1707@gmail.com>
2024-01-29 23:15:32 +0700

    feat(stock report): filter get  report

--- routes/report-stocks.php
+++ routes/report-stocks.php
@@ -5,4 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/report-stocks')->group(function () {
     Route::get('/', [StockReportController::class, 'index']);
+    Route::get('/today', [StockReportController::class, 'getReportsToday']);
+    Route::get('/month/{month}', [StockReportController::class, 'getReportsMonth']);
+    Route::get('/year/{year}', [StockReportController::class, 'getReportsYear']);
+    Route::get('/date/{date}', [StockReportController::class, 'customGetReports ']);
 });
