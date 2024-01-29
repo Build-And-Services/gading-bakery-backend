160acb6812aaba7e18c58e9a273026aa07a61057
rafikusdiarto <rafikusdiarto1707@gmail.com>
2024-01-29 20:15:04 +0700

    feat(report): stock report

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::prefix('/v1')->group(function () {
 
     include __DIR__ . '/dashboard.php';
     include __DIR__ . '/report.php';
+    include __DIR__ . '/report-stocks.php';
 
     Route::prefix('/stock')->group(function () {
         // isi sesua controller masing masing
