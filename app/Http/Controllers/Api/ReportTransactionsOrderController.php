d30f011e5aa6f2618883baed6f48272abaaf5989
Faisal Bahari <fbhr1112@gmail.com>
2024-04-29 19:55:06 +0700

    fix: report excel response

--- app/Http/Controllers/Api/ReportTransactionsOrderController.php
+++ app/Http/Controllers/Api/ReportTransactionsOrderController.php
@@ -15,10 +15,29 @@ class ReportTransactionsOrderController extends Controller
         try {
             $data = DB::table('order_items')->select('product_name', 'selling_price', DB::raw('DATE_FORMAT(created_at, "%e %b %Y" ) as tanggal'), 'quantity')
                 ->get();
+
+            $formattedData = [];
+            $totalPrice = 0;
+
+            foreach ($data as $item) {
+                for ($i = 0; $i < $item->quantity; $i++) {
+                    array_push($formattedData, [
+                        'product_name' => $item->product_name,
+                        'selling_price' => $item->selling_price,
+                        'tanggal' => $item->tanggal,
+                        'quantity' => 1
+                    ]);
+                    $totalPrice += $item->selling_price;
+                }
+            }
+
             return response()->json([
                 'status' => true,
                 'message' => 'berhasil',
-                'data' => $data,
+                'data' => [
+                    'report' => $formattedData,
+                    'total' => $totalPrice
+                ],
             ]);
         } catch (\Throwable $e) {
             return response()->json($e->getMessage(), 400);
@@ -32,10 +51,29 @@ class ReportTransactionsOrderController extends Controller
                 ->whereMonth('created_at', $month)
                 ->whereDay('created_at', $day)
                 ->get();
+
+            $formattedData = [];
+            $totalPrice = 0;
+
+            foreach ($data as $item) {
+                for ($i = 0; $i < $item->quantity; $i++) {
+                    array_push($formattedData, [
+                        'product_name' => $item->product_name,
+                        'selling_price' => $item->selling_price,
+                        'tanggal' => $item->tanggal,
+                        'quantity' => 1
+                    ]);
+                    $totalPrice += $item->selling_price;
+                }
+            }
+
             return response()->json([
                 'status' => true,
                 'message' => 'berhasil',
-                'data' => $data,
+                'data' => [
+                    'report' => $formattedData,
+                    'total' => $totalPrice
+                ],
             ]);
         } catch (\Throwable $e) {
             return response()->json($e->getMessage(), 400);
@@ -48,10 +86,29 @@ class ReportTransactionsOrderController extends Controller
                 ->whereYear('created_at', $year)
                 ->whereMonth('created_at', $month)
                 ->get();
+
+            $formattedData = [];
+            $totalPrice = 0;
+
+            foreach ($data as $item) {
+                for ($i = 0; $i < $item->quantity; $i++) {
+                    array_push($formattedData, [
+                        'product_name' => $item->product_name,
+                        'selling_price' => $item->selling_price,
+                        'tanggal' => $item->tanggal,
+                        'quantity' => 1
+                    ]);
+                    $totalPrice += $item->selling_price;
+                }
+            }
+
             return response()->json([
                 'status' => true,
                 'message' => 'berhasil',
-                'data' => $data,
+                'data' => [
+                    'report' => $formattedData,
+                    'total' => $totalPrice
+                ],
             ]);
         } catch (\Throwable $e) {
             return response()->json($e->getMessage(), 400);
@@ -63,10 +120,29 @@ class ReportTransactionsOrderController extends Controller
             $data = DB::table('order_items')->select('product_name', 'selling_price', DB::raw('DATE_FORMAT(created_at, "%e %b %Y" ) as tanggal'), 'quantity')
                 ->whereYear('created_at', $year)
                 ->get();
+
+            $formattedData = [];
+            $totalPrice = 0;
+
+            foreach ($data as $item) {
+                for ($i = 0; $i < $item->quantity; $i++) {
+                    array_push($formattedData, [
+                        'product_name' => $item->product_name,
+                        'selling_price' => $item->selling_price,
+                        'tanggal' => $item->tanggal,
+                        'quantity' => 1
+                    ]);
+                    $totalPrice += $item->selling_price;
+                }
+            }
+
             return response()->json([
                 'status' => true,
                 'message' => 'berhasil',
-                'data' => $data,
+                'data' => [
+                    'report' => $formattedData,
+                    'total' => $totalPrice
+                ],
             ]);
         } catch (\Throwable $e) {
             return response()->json($e->getMessage(), 400);
